2c9471793cf0399c70e2125a61728ab226aa1351
N-M-D <cty.chuateckyong@gmail.com>
2022-09-06 15:17:02 +0800

    Added v1 of pfp update

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -30,8 +30,20 @@ const Profile = () => {
             { headers: {'Content-Type': 'multipart/form-data', 
                 'Authorization': 'Bearer ' + token}}
         )
-        console.log(result)
+        console.log(result);
+        alert("Done");
     }
+
+    // async function updatePfp(e){
+    //     e.preventDefault();
+    //     const formData = new FormData();
+    //     formData.append("pfp", pfp);
+    //     const result = await axios.put(`${heroku}/user/update`, formData, 
+    //         { headers: {'Content-Type': 'multipart/form-data', 
+    //             'Authorization': 'Bearer ' + token}}
+    //     )
+    //     console.log(result)
+    // }
 
     useEffect(() => {
         if(token == null){
@@ -49,25 +61,35 @@ const Profile = () => {
         })
         .then((response) => {
             let user = response.rows[0];
+            console.log("User: ", user);
             setUsername(user.username);
             setEmail(user.email);
             setPfp(user.pfp);
+            const pfpDiv = document.getElementById("pfp");
+            pfpDiv.style.backgroundImage = `url('${pfp}')`;
+            console.log(pfp)
         })
-    }, [])
+    }, [pfp])
 
     return ( 
-        <div className="container">
-            
+        <div className="container" style={{border: "1px solid green"}}>
+            <div id="modal" style={{backgroundColor: "#222", display: "none", zIndex: "1", position: "fixed", color: "white", padding: "1em", width: "calc(100%-15px)"}}>
+                <span class="close" onClick={hideModal}>&times;</span>
+                <h4 className="header-4">Edit Profile Picture</h4>
+                <form>
+                    <div className="form-group">
+                        <label>Profile Picture</label>
+                        <input id="email" type="file" className="form-control" accept="image/*" onChange={event => setPfp(event.target.files[0])}></input>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Edit</button>
+                </form>
+            </div>
             <h1 className="heading">Profile</h1>
             <div className="main">
-            <div id="modal" style={{backgroundColor: "#222", display: "none", zIndex: "1", position: "fixed", width: "50vw", margin: "auto"}}>
-                <span class="close" onClick={hideModal}>&times;</span>
-                <p>Some text in the Modal..</p>
-            </div>
                 <form onSubmit={updateDetails}>
                     <div className="row">
                         <div className="col col-md-6 col-6">
-                            <div className="pfp" style={{backgroundColor: "#222", borderRadius:"100vmax", width: "100%", height: 0, paddingBottom: "100%", backgroundImage: "url('https://chuashopping.s3.ap-southeast-1.amazonaws.com/ada556a4f28e7f21f00ea1779f56bd5b')", backgroundPosition: "center"}} onClick={showModal}></div>
+                            <div className="pfp" id="pfp" style={{backgroundColor: "#222", borderRadius:"100vmax", width: "100%", height: 0, paddingBottom: "100%", backgroundImage: "url('https://chuashopping.s3.ap-southeast-1.amazonaws.com/ada556a4f28e7f21f00ea1779f56bd5b')", backgroundPosition: "center"}} onClick={showModal}></div>
                         </div>
                         <div className="col col-md-6 col-6">
                             <div className="form-group">
